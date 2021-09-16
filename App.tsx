24595703aeca4b4510027d5c1618dadf15c60222
sagar-github-2021 <sagar8653170300@gmail.com>
2021-09-16 18:23:18 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+// import './styles.css';
+// import MouseHover from "./Components/MouseHover";
+
+export default function App() {
+  return <div className="App">hello</div>;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-hqxgpn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-hqxgpn)
