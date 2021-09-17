e5ab9cc3b5634cc095e574eed63a1c4bcbe38086
sagar-github-2021 <sagar8653170300@gmail.com>
2021-09-17 10:11:28 +0530

    structure read for TS

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-// import './styles.css';
-// import MouseHover from "./Components/MouseHover";
+import './style.css';
 
 export default function App() {
   return <div className="App">hello</div>;
