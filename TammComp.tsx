43659dee3a9d383ebd9cc6cd5ddba71312adbee1
sagar-github-2021 <sagar8653170300@gmail.com>
2021-09-19 06:06:48 +0530

    3 files modified, 3 added, 1 deleted

---
+++ TammComp.tsx
@@ -0,0 +1,28 @@
+// import React, { useState } from 'react';
+// import './style.css';
+
+// //here we are using properties of mouse hover from HOC alongwith additional data (isHovered and data)
+// export default function ProjectComponent(props) {
+//   const hoverText = props.isHovered ? 'IN' : 'Out';
+//   let green = '#11e619';
+//   let red = '#9a0426';
+//   const [colorCode, setColor] = useState('#11e6');
+
+//   function colorChange(e) {
+//     const newColor = colorCode === green ? red : green;
+//     console.log('oldColor-', colorCode);
+//     console.log('newColor-', newColor);
+//     setColor(newColor);
+//   }
+
+//   return (
+//     <div>
+//       <h3>TAMM Component using StackBlitz..</h3>
+//       {console.log('props :', props)}
+//       {console.log('props-data-name :', props.data[0].name)}
+//       <h2 {...props} style={{ color: colorCode }} onClick={colorChange}>
+//         Mouse is Hover {hoverText} to Component.
+//       </h2>
+//     </div>
+//   );
+// }
---
+++ server/server.tsx
@@ -0,0 +1,21 @@
+// const express = require('express');
+// const session = require('express-session');
+// const app = express();
+// app.use(express.bodyParser());
+
+// const routes = require('./routes');
+// app.use(express.json());
+// app.use('/pub/proxy', routes);
+// app.use('/api/proxy', routes);
+
+// app.use(
+//   session({
+//     secret: 'absdajfaldsjflasdj',
+//     resave: false,
+//     saveUninitialized: false,
+//   })
+// );
+
+// app.get('/', (req, res) => {
+//   console.log('you are on root page');
+// });
