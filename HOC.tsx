9b164da98b5bc9a7d18b7e621d7e621e58740933
sagar-github-2021 <sagar8653170300@gmail.com>
2021-09-19 10:15:38 +0530

    1 file modified

--- HOC.tsx
+++ HOC.tsx
@@ -2,6 +2,9 @@
 // import './style.css';
 
 // export default function Hoc(HocComponent, data) {
+//   interface WithLoadingProps {
+//     isHovered: boolean;
+//   }
 //   const [isHovered, setHover] = React.useState(false);
 
 //   return class extends Component {
@@ -25,3 +28,37 @@
 //     }
 //   };
 // }
+import React, { Component } from 'react';
+import './style.css';
+
+export interface WithLoadingProps {
+  isHovered: boolean;
+}
+
+export default function Hoc<P extends WithLoadingProps>(
+  HocComponent: Component<P>,
+  data
+) {
+  const [isHovered, setHover] = React.useState(false);
+
+  return class extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        data: data,
+      };
+    }
+
+    render() {
+      return (
+        <HocComponent
+          data={this.state.data}
+          {...this.props}
+          onMouseOver={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+          isHovered={isHovered}
+        />
+      );
+    }
+  };
+}
